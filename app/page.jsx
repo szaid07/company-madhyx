a7b4badc1bf594847699896e33dea21ef0e8fbb3
Shaikh Zaid <szaid07@gmail.com>
2025-08-04 18:37:44 +0400

    Update dependencies in yarn.lock to use npm registry, change page title to "Madhyx Technologies", and update favicon.ico

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Home1 from "./(homes)/home-1/page";
 export const metadata = {
-  title: "Home 1 || Frisk - Creative Agency & Portfolio Nextjs Template",
+  title: "Madhyx Technologies",
 };
 export default function Home() {
   return (
